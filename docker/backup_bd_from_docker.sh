22706935290e967bbd7fff79e13c2914c62d7d23
sergey.alekseev <demiurg58@gmail.com>
2024-08-19 17:29:26 +0300

    add backup script

--- docker/backup_bd_from_docker.sh
+++ docker/backup_bd_from_docker.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #Создание бэкапа Stable
-#docker exec backend-mvp-stable_db pg_dump -U mycompanybd_user mycompanybd > /root/backup_pgs_mvp_stable/$(date +%d-%b-%Y).dump
-#docker exec backend-mvp-develop_db pg_dump -U mycompanybd_user mycompanybd > /root/backup_pgs_mvp_develop/$(date +%d-%b-%Y).dump
+#docker exec CONTAINER_NAME pg_dump -U user_name bd_name > /root/backup_pgs_mvp_stable/$(date +%d-%b-%Y).dump
+#docker exec CONTAINER_NAME pg_dump -U user_name bd_name > /root/backup_pgs_mvp_develop/$(date +%d-%b-%Y).dump
 echo -e "\nСоздание бэкапа develop_db\n"
-docker exec develop_db pg_dump -U mycompanybd_user mycompanybd >/root/backup_pgs_develop/$(date +%d-%b-%Y).dump
+docker exec CONTAINER_NAME pg_dump -U user_name bd_name >/root/backup_pgs_develop/$(date +%d-%b-%Y).dump
 
 #Создание бэкапа всех веток, которые начинаются со слова Release
 echo -e "\nСоздание бэкапа всех веток, которые начинаются со слова release\n"
 for line in $(docker ps -a --format "{{.Names}}"); do if [[ "$line" == "release-"* ]] && [[ "$line" == *"_db" ]]; then
   echo "$line"
   if [ ! -d "/root/backup_pgs_releases/$line" ]; then mkdir /root/backup_pgs_releases/$line; fi
-  docker exec $line pg_dump -U mycompanybd_user mycompanybd >/root/backup_pgs_releases/$line/$(date +%d-%b-%Y).dump
+  docker exec $line pg_dump -U user_name bd_name >/root/backup_pgs_releases/$line/$(date +%d-%b-%Y).dump
 fi; done
 
 #Удаление старых бэкапов
--- docker/backup_pgs.sh
+++ docker/backup_pgs.sh
@@ -1,9 +1,7 @@
-docker exec backend-mvp-develop_db pg_dump -U mycompanybd_user mycompanybd > /root/backup_pgs_mvp_develop/$(date +%d-%b-%Y).dump
-docker exec develop_db pg_dump -U mycompanybd_user mycompanybd > /root/backup_pgs_develop/$(date +%d-%b-%Y).dump
+docker exec CONTAINER_NAME pg_dump -U user_name bd_name > /root/backup_pgs_mvp_develop/$(date +%d-%b-%Y).dump
+docker exec CONTAINER_NAME pg_dump -U user_name bd_name > /root/backup_pgs_develop/$(date +%d-%b-%Y).dump
 
 #Удаление старых бэкапов
 find /root/backup_pgs_mvp_stable -mindepth 1 -mtime +30 -delete
 find /root/backup_pgs_mvp_develop -mindepth 1 -mtime +30 -delete
 find /root/backup_pgs_develop -mindepth 1 -mtime +30 -delete
-
-
